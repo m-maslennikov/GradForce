d36ac45563ab09441a6a7820c835ad7be02e44d3
aslazaro <aslazaropldt@gmail.com>
2019-08-30 16:51:08 +1200

    Profil Add, Edit and Delete done

--- routes/rootRoutes.js
+++ routes/rootRoutes.js
@@ -12,7 +12,11 @@ router.get('/dashboard', isAuth, controller.getDashboard);
 router.get('/profile', isAuth, controller.getMyProfile);
 router.post('/profile/my/save', isAuth, controller.saveMyProfile);
 router.post('/profile/my/education', isAuth, controller.saveMyEducation);
+router.post('/profile/my/edit-education', isAuth, controller.editMyEducation);
+router.post('/profile/my/delete-education', isAuth, controller.deleteMyEducation);
 router.post('/profile/my/work', isAuth, controller.saveMyWork);
+router.post('/profile/my/edit-work', isAuth, controller.editMyWork);
+router.post('/profile/my/delete-work', isAuth, controller.deleteMyWork);
 router.post('/profile/skill/test', isAuth, controller.generateTestLink);
 
 router.get('/skills', isAuth, controller.getSkills);
